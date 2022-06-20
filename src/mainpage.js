518cb17fd351b7fcf59af6471a2989c2a1ba65e2
C-Stillwell <danceswithhodge@gmail.com>
2022-06-20 14:30:29 +0900

    Revert "Move sidebar toggle event listener"
    This reverts commit bd35dcb1c2c9dad8ba85a23a230f8d5f7930333c.

--- src/mainpage.js
+++ src/mainpage.js
@@ -19,6 +19,7 @@ export default class Mainpage {
     initiateEventListeners() {
         this.addTaskButtonEvent()
         this.addMainBtnEvent()
+        this.addSideMenuEvent()
         this.addNewListBtnEvent()
         this.addDeleteNewListBtnEvent()
         this.addDeleteAllEvent()
@@ -227,8 +228,6 @@ export default class Mainpage {
         sideMenuBtnImg.id = 'open-side-menu'
         sideMenuBtnImg.setAttribute('alt', 'Open side menu button')
         sideMenuBtnContainer.appendChild(sideMenuBtnImg)
-
-        this.addSideMenuEvent()
     }
 
     
